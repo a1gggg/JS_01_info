7bf2348e23ae8c80f9721d597be72935da460c37
Ruslan_PM <velikoedelanie@gmail.com>
2025-05-21 23:26:41 +0300

    210520252326

--- javascript/TestJS/cript_TestJS.js
+++ javascript/TestJS/cript_TestJS.js
@@ -1,3 +1,12 @@
 "use strict"
 // Суворий режим перевірки
 
+ let value1 = prompt ('Enter value1', '');
+ let value2 = value1??'default value';
+ if (value2 =='') {
+  value2 = 'default value'
+ }
+ alert (value2);
+
+
+
